e03251d46cd1866acd351105c9d7694b35e43ba4
Abhineel-Nandi <noobNandi04@gmail.com>
2020-06-20 12:19:12 +0530

    Create ZCR.cpp

---
+++ ZCR.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int me = 1025;
+
+int T, N, K;
+double p;
+double dp[me][me];
+
+int main(int argc, const char * argv[]) {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    
+    cin >> T;
+    while(T --){
+        cin >> N >> K;
+        for(int i = 1; i <= N; i ++)
+            for(int j = 0; j <= i; j ++)
+                dp[i][j] = 0;
+        dp[0][0] = 1.;
+        for(int i = 1; i <= N; i ++){
+            cin >> p;
+            dp[i][0] = dp[i - 1][0] * (1 - p);
+            for(int j = 1; j <= i; j ++){
+                dp[i][j] = dp[i - 1][j - 1] * p + dp[i - 1][j] * (1 - p);
+            }
+        }
+        cout.precision(10);
+        cout << fixed << dp[N][K] << endl;
+    }
+    
+    return 0;
+}
