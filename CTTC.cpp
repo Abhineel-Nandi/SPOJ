35647715481d1e0b97df33f64bd454cc6559d917
Abhineel-Nandi <noobNandi04@gmail.com>
2020-06-20 12:16:43 +0530

    Create CTTC.cpp

---
+++ CTTC.cpp
@@ -0,0 +1,47 @@
+#include <bits.stdc++.h>
+using namespace std;
+
+const int S = 1 << 10;
+
+int T, N;
+int order[S];
+int was[S], children[S];
+
+int main(int argc, const char * argv[]) {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    
+    cin >> T;
+    for (int c = 0; c < T; c ++) {
+        cin >> N;
+        for (int i = 1; i <= N + N; i ++) {
+            cin >> order[i];
+        }
+        for (int i = 1; i <= N; i ++) {
+            children[i] = 0;
+            was[i] = 0;
+        }
+        stack<int> open;
+        for (int i = 1; i <= N + N; i ++) {
+            if (!was[order[i]]) {
+                if (!open.empty()) {
+                    children[open.top()] ++;
+                }
+                was[order[i]] = 1;
+                open.push(order[i]);
+            } else {
+                open.pop();
+            }
+        }
+        cout << "Case " << c + 1 << ":" << endl;
+        for (int i = 1; i <= N; i ++) {
+            cout << i << " -> " << children[i] << endl;
+        }
+        if (c < T - 1) {
+            cout << endl;
+        }
+    }
+    
+    return 0;
+}
+
